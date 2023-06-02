16d5f5ed4f7d2758a35f9a0c5d9b99d2f1edd12b
CharlesPikachu <1159254961@qq.com>
2023-06-02 23:23:34 +0800

    fix params_constructor

--- csseg/modules/models/optimizers/builder.py
+++ csseg/modules/models/optimizers/builder.py
@@ -12,7 +12,7 @@ from .constructors import DefaultParamsConstructor
 '''BuildOptimizer'''
 def BuildOptimizer(model, optimizer_cfg):
     optimizer_cfg = copy.deepcopy(optimizer_cfg)
-    constructor_cfg = optimizer_cfg.pop('constructor_cfg')
+    params_constructor_cfg = optimizer_cfg.pop('params_constructor_cfg')
     # supported optimizers
     supported_optimizers = {
         'SGD': torch.optim.SGD,
@@ -25,11 +25,11 @@ def BuildOptimizer(model, optimizer_cfg):
     }
     # parse
     optimizer_type = optimizer_cfg.pop('type')
-    constructor_type = constructor_cfg.pop('type')
+    params_constructor_type = params_constructor_cfg.pop('type')
     supported_paramsconstructors = {
         'DefaultParamsConstructor': DefaultParamsConstructor
     }
-    params_constructor = supported_paramsconstructors[constructor_type](optimizer_cfg, **constructor_cfg)
+    params_constructor = supported_paramsconstructors[params_constructor_type](optimizer_cfg, **params_constructor_cfg)
     optimizer_cfg['params'] = params_constructor(model)
     optimizer = supported_optimizers[optimizer_type](**optimizer_cfg)
     # return
